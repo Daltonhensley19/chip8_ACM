87dbdbe3b76cb82e45d9cbfe7da1c565db91744a
Daltonhensley19 <Daltonhensley2000@gmail.com>
2021-01-09 19:14:25 -0500

    Made some of the comments more accurate and clear.

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ uint8_t keymap[16] = {
 
 int main(int argc, char **argv) {
 
-    // Command usage
+    // Display terminal usage
     if (argc != 2) {
         cout << "Usage: chip8 <ROM file>" << endl;
         return 1;
@@ -42,15 +42,15 @@ int main(int argc, char **argv) {
     int w = 1024;                   // Window width
     int h = 512;                    // Window height
 
-    // The window we'll be rendering to
+    // First, we will create a window
     SDL_Window* window = NULL;
 
-    // Initialize SDL
+    // Check to see if SDL can initialize
     if ( SDL_Init(SDL_INIT_EVERYTHING) < 0 ) {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
         exit(1);
     }
-    // Create window
+    
     window = SDL_CreateWindow(
             "CHIP-8 Emulator",
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -62,7 +62,7 @@ int main(int argc, char **argv) {
         exit(2);
     }
 
-    // Create renderer
+    // We then create the renderer for the window
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_RenderSetLogicalSize(renderer, w, h);
 
@@ -72,7 +72,7 @@ int main(int argc, char **argv) {
             SDL_TEXTUREACCESS_STREAMING,
             64, 32);
 
-    // Temporary pixel buffer
+    // Pixel buffer
     uint32_t pixels[2048];
 
 
@@ -85,7 +85,7 @@ int main(int argc, char **argv) {
     while (true) {
         chip8.execute_cycle();
 
-        // Process SDL events
+        // Process SDL events, such as the keyboard
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) exit(0);
@@ -96,8 +96,7 @@ int main(int argc, char **argv) {
                     exit(0);
 
                 if (e.key.keysym.sym == SDLK_F1)
-                    goto load;      // *gasp*, a goto statement!
-                                    // Used to reset/reload ROM
+                    goto load;    
 
                 for (int i = 0; i < 16; ++i) {
                     if (e.key.keysym.sym == keymap[i]) {
@@ -119,21 +118,21 @@ int main(int argc, char **argv) {
         if (chip8.drawFlag) {
             chip8.drawFlag = false;
 
-            // Store pixels in temporary buffer
+            // We will then store pixels in the temporary buffer
             for (int i = 0; i < 2048; ++i) {
                 uint8_t pixel = chip8.gfx[i];
                 pixels[i] = (0x00FFFFFF * pixel) | 0xFF000000;
             }
             // Update SDL texture
             SDL_UpdateTexture(sdlTexture, NULL, pixels, 64 * sizeof(Uint32));
-            // Clear screen and render
+            // Cleanup
             SDL_RenderClear(renderer);
             SDL_RenderCopy(renderer, sdlTexture, NULL, NULL);
             SDL_RenderPresent(renderer);
         }
 
-        // Sleep to slow down emulation speed
-        std::this_thread::sleep_for(std::chrono::microseconds(1200));
+        // Slow down the running thread to pace the emulation 
+        std::this_thread::sleep_for(std::chrono::microseconds(1500));
 
     }
 }
